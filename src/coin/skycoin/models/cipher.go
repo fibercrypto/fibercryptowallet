2a11d430aee27c2376fa5449a376a4ece111e475
Olemis Lang <olemis@simelo.tech>
2019-07-25 18:27:02 -0400

    [skycoin] refs #45 - Add mock class implemeing Skycoin cipher address and iterator

---
+++ src/coin/skycoin/models/cipher.go
@@ -0,0 +1,41 @@
+package skycoin
+
+import (
+	"github.com/FiberCrypto/FiberCryptoWallet/src/core"
+)
+
+type SkycoinAddress struct {
+}
+
+func (*SkycoinAddress) IsBip32() bool {
+	return false
+}
+
+func (*SkycoinAddress) String() string {
+	return "249iEtdhniFppBTpkzq7syoiBaydLi6USnU"
+}
+
+type SkycoinAddressIterator struct {
+	index int
+	value SkycoinAddress
+}
+
+func (iter *SkycoinAddressIterator) Value() core.Address {
+	if index == 0 && iter.value == nil {
+		iter.value = &SkycoinTransactionInput{}
+	}
+	return iter.value
+}
+
+func (iter *SkycoinAddressIterator) Next() bool {
+	if iter.index < 3 {
+		iter.value = &SkycoinAddress{}
+		iter.index++
+		return true
+	}
+	return false
+}
+
+func (iter *SkycoinAddressIterator) HasNext() bool {
+	return iter.index < 3
+}
