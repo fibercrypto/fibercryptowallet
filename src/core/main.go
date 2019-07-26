be3ca7d185f39842ba8b872c7f6e160df40ea9af
Olemis Lang <olemis@simelo.tech>
2019-07-26 11:58:17 -0400

    [core] [util] refs #56 - AlcoinManager. Dynamic plugin registry. Helper funcs

---
+++ src/core/main.go
@@ -0,0 +1,74 @@
+package core
+
+type AltcoinMetadata struct {
+	Name          string
+	Ticker        string
+	Family        string
+	HasBip44      bool
+	Bip44CoinType int
+}
+
+type AltcoinPlugin interface {
+	ListSupportedAltcoins() []AltcoinMetadata
+	ListSupportedFamilies() []string
+	RegisterTo(manager AltcoinManager)
+	GetName() string
+	GetDescription() string
+	LoadWalletEnvs() []WalletEnv
+}
+
+type AltcoinManager interface {
+	RegisterPlugin(p AltcoinPlugin)
+	RegisterAltcoin(ticker, info AltcoinMetadata, plugin AltcoinPlugin)
+	ListRegisteredPlugins() []AltcoinPlugin
+	LookupAltcoinManager(ticker string) (AltcoinPlugin, bool)
+	DescribeAltcoin(ticker string) (AltcoinMetadata, bool)
+}
+
+type altcoinRecord struct {
+	Manager  AltcoinPlugin
+	Metadata AltcoinMetadata
+}
+
+// fibercoinAltcoinManager is a singleton class
+type fibercryptoAltcoinManager struct {
+	registeredPlugins []AltcoinPlugin
+	altcoinMap        map[string]altcoinRecord
+}
+
+var (
+	manager fibercryptoAltcoinManager
+)
+
+func (m *fibercryptoAltcoinManager) RegisterPlugin(p AltcoinPlugin) {
+	p.RegisterTo(m)
+}
+
+func (m *fibercryptoAltcoinManager) RegisterAltcoin(ticker, info AltcoinMetadata, plugin AltcoinPlugin) {
+	m.altcoinMap[info.Ticker] = altcoinRecord{
+		Manager:  plugin,
+		Metadata: info,
+	}
+}
+
+func (m *fibercryptoAltcoinManager) ListRegisteredPlugins() []AltcoinPlugin {
+	return m.registeredPlugins
+}
+
+func (m *fibercryptoAltcoinManager) LookupAltcoinManager(ticker string) (AltcoinPlugin, bool) {
+	if r, isRegistered := m.altcoinMap[ticker]; isRegistered {
+		return r.Manager, true
+	}
+	return nil, false
+}
+
+func (m *fibercryptoAltcoinManager) DescribeAltcoin(ticker string) (AltcoinMetadata, bool) {
+	if r, isRegistered := m.altcoinMap[ticker]; isRegistered {
+		return r.Metadata, true
+	}
+	return AltcoinMetadata{}, false
+}
+
+func LoadAltcoinManager() AltcoinManager {
+	return &manager
+}
