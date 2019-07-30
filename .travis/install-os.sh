ef30294ecee39cd75050b87de99decaa1fa7fc84
Marcos Maceo <marcosmaceowork@gmail.com>
2019-07-30 15:55:14 -0400

    [ci] refs #2 Remove setup installation linux and add env var in MacOs

--- .travis/install-os.sh
+++ .travis/install-os.sh
@@ -2,20 +2,15 @@
 
 brew update
 brew install p7zip | true
-brew install wget | true
 
-brew install pyenv
 # Add pyenv initializer to shell startup script
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+echo "~/5.13.0/gcc_64/bin"      >> ~/.bash_profile
 # Reload your profile
 source ~/.bash_profile
-pyenv install 3.6.8
-
-pyenv global 3.6.8
 
 pip install requests
 
 (cd $HOME && wget https://git.kaidan.im/lnj/qli-installer/raw/master/qli-installer.py && chmod +x qli-installer.py && ./qli-installer.py 5.13.0 mac desktop)
 
-echo "5.13.0/gcc_64/bin" >>  /etc/paths
 
