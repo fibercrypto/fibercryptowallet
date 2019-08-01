58cff2e93b2cc2aeed5ec03807e8afac1d0f2cb2
TNO <mtpotro41@gmail.com>
2019-08-01 15:48:49 -0400

    [outputs] refs #47 - Implement TransactionOutput and iterator interfaces for skycoin.

---
+++ src/coin/skycoin/models/coin.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"github.com/fibercrypto/FiberCryptoWallet/src/core"
+	"github.com/fibercrypto/FiberCryptoWallet/src/util"
+)
+
+type SkycoinTransactionOutput struct { //Implements TransactionOutput interface
+	Id 		  string
+	Address   SkycoinAddress
+	Sky 	  uint64
+	CoinHours uint64
+}
+
+func (sto SkycoinTransactionOutput) GetId() string {
+	return sto.Id
+}
+
+func (sto SkycoinTransactionOutput) GetAddress() Address {
+	return sto.Address
+}
+
+func (sto SkycoinTransactionOutput) GetCoins(ticker string) uint64 {
+	if ticker == "Sky" {
+		return sto.Sky
+	}
+	return sto.CoinHours
+}
+
+type SkycoinTransactionOutputIterator struct { //Implements TransactionOutputIterator interface
+	Current interface
+	Outputs []SkycoinTransactionOutput
+}
+
+func (it SkycoinTransactionOutputIterator) Value() core.TransactionOutput {
+	return it.Outputs[it.Current]
+}
+
+func (it *SkycoinTransactionOutputIterator) Next() bool {
+	if it.HasNext() {
+		it.Current++
+		return true
+	}
+	return false
+}
+
+func (it SkycoinTransactionOutputIterator) HasNext() bool {
+	return (it.Current + 1) < len(it.Outputs)
+}
