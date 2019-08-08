d7c0aa5b1eabca2380775214e67c515c774b9a54
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2019-08-08 17:33:21 -0400

    [blockchain] refs #46 Function to obtain the coin value

--- src/util/util.go
+++ src/util/util.go
@@ -1,8 +1,15 @@
 package util
 
+import "strconv"
+
 func Min(a, b int) int {
 	if a <= b {
 		return a
 	}
 	return b
+}
+
+func GetCoinValue(value string) {
+	coin, err := strconv.ParseFloat(value, 64)
+	return uint64(coin * 1000000), err
 }
