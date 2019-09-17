449d5283815e9f63f282904424c30b6f0ea81c53
Mauricio Perdomo <ml.perdomo97@gmail.com>
2019-09-17 16:44:08 -0400

    [ui][txns] Set model property of wallets and addresses in advance send page

--- src/models/Outputs.go
+++
@@ -1,7 +0,0 @@
-package models
-
-func init() {
-	ModelWallets_QmlRegisterType2("OutputsModels", 1, 0, "QWallets")
-	ModelAddresses_QmlRegisterType2("OutputsModels", 1, 0, "QAddresses")
-	ModelOutputs_QmlRegisterType2("OutputsModels", 1, 0, "QOutputs")
-}
--- src/models/models.go
+++ src/models/models.go
@@ -10,4 +10,7 @@ func init() {
 	ConfigManager_QmlRegisterType2("Config", 1, 0, "ConfigManager")
 	WalletSource_QmlRegisterType2("Config", 1, 0, "WalletSource")
 	ModelManager_QmlRegisterType2("WalletsManager", 1, 0, "ModelManager")
+	ModelWallets_QmlRegisterType2("OutputsModels", 1, 0, "QWallets")
+	ModelAddresses_QmlRegisterType2("OutputsModels", 1, 0, "QAddresses")
+	ModelOutputs_QmlRegisterType2("OutputsModels", 1, 0, "QOutputs")
 }
