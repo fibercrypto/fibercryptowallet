5e7e911deea6cf635bed418ef72d7f22e307ed51
Olemis Lang <olemis@simelo.tech>
2019-09-26 16:54:35 -0400

    [core] refs #142 - Docstrings for CryptoAccount, Address, AddressIterator, TxnSignerIterator

--- src/core/cipher.go
+++ src/core/cipher.go
@@ -1,14 +1,22 @@
 package core
 
+// Address identifier for sending and receiving transactions
 type Address interface {
+	// IsBip32 flaqg shall be set if adddress generation complies to BIP 32
 	IsBip32() bool
+	// String return human-readable representation of this address
 	String() string
+	// GetCryptoAccount provides access to address transaction history
 	GetCryptoAccount() CryptoAccount
 }
 
+// Iterate over addresses in a container
 type AddressIterator interface {
+	// Value of address at iterator pointer position
 	Value() Address
+	// Next discards current value and moves iteration pointer up to next item
 	Next() bool
+	// HasNext may be used to query whether more items are to be expected in teh sequence
 	HasNext() bool
 }
 
@@ -22,9 +30,14 @@ type TxnSigner interface {
 	GetSignerDescription() string
 }
 
+// TxSignerIterator enumerates a set if TxSigner strategies
 type TxnSignerIterator interface {
+	// Value of signer at iterator pointer position
 	Value() TxnSigner
+	// Next discards current value and moves iteration pointer up to next item
 	Next() bool
+	// HasNext may be used to query whether more items are to be expected in teh sequence
 	HasNext() bool
+	// Count total number of items in sequence
 	Count() int
 }
