4e7e46e8b93f348763aa66909ac5e77f4d7e88d0
Marcos Maceo <marcosmaceowork@gmail.com>
2019-10-13 18:46:07 -0400

    [coin][sky] refs #115 Add logger for coin model in Skycoin

--- src/coin/skycoin/models/coin.go
+++ src/coin/skycoin/models/coin.go
@@ -2,37 +2,46 @@ package skycoin
 
 import (
 	"fmt"
+	"github.com/fibercrypto/FiberCryptoWallet/src/util/logging"
 	"strconv"
 	"time"
 
-	"github.com/skycoin/skycoin/src/cipher"
-	"github.com/skycoin/skycoin/src/visor"
-	"github.com/skycoin/skycoin/src/coin"
 	"github.com/fibercrypto/FiberCryptoWallet/src/core"
 	"github.com/fibercrypto/FiberCryptoWallet/src/util"
+	"github.com/skycoin/skycoin/src/cipher"
+	"github.com/skycoin/skycoin/src/coin"
 	"github.com/skycoin/skycoin/src/readable"
+	"github.com/skycoin/skycoin/src/visor"
 )
 
+var logCoin = logging.MustGetLogger("Skycoin coin")
+
 /*
-SkycoinPendingTransaction
+	SkycoinPendingTransaction
+
+	Implements Transaction interface
 */
-type SkycoinPendingTransaction struct { //Implements Transaction interface
+type SkycoinPendingTransaction struct {
 	Transaction readable.UnconfirmedTransactionVerbose
 }
 
 func (txn *SkycoinPendingTransaction) SupportedAssets() []string {
+	logCoin.Info("Getting supported assets")
 	return []string{Sky, CoinHour, CalculatedHour}
 }
 
 func (txn *SkycoinPendingTransaction) GetTimestamp() core.Timestamp {
+	logCoin.Info("Getting timestamp")
 	return core.Timestamp(txn.Transaction.Received.Unix())
 }
 
 func (txn *SkycoinPendingTransaction) GetStatus() core.TransactionStatus {
+	logCoin.Info("Getting status")
 	return core.TXN_STATUS_PENDING
 }
 
 func (txn *SkycoinPendingTransaction) GetInputs() []core.TransactionInput {
+	logCoin.Info("Getting inputs from Skycoin pending transaction")
 	inputs := make([]core.TransactionInput, 0)
 	for _, input := range txn.Transaction.Transaction.In {
 		inputs = append(inputs, &SkycoinPendingTransactionInput{Input: input})
@@ -41,6 +50,7 @@ func (txn *SkycoinPendingTransaction) GetInputs() []core.TransactionInput {
 }
 
 func (txn *SkycoinPendingTransaction) GetOutputs() []core.TransactionOutput {
+	logCoin.Info("Getting outputs from Skycoin pending transaction")
 	outputs := make([]core.TransactionOutput, 0)
 	for _, output := range txn.Transaction.Transaction.Out {
 		outputs = append(outputs, &SkycoinPendingTransactionOutput{Output: output})
@@ -49,10 +59,12 @@ func (txn *SkycoinPendingTransaction) GetOutputs() []core.TransactionOutput {
 }
 
 func (txn *SkycoinPendingTransaction) GetId() string {
+	logCoin.Info("Getting id of pending transaction")
 	return txn.Transaction.Transaction.Hash
 }
 
 func (txn *SkycoinPendingTransaction) ComputeFee(ticker string) (uint64, error) {
+	logCoin.Info("Computing fee for " + ticker + " ticket")
 	if ticker == CoinHour {
 		return txn.Transaction.Transaction.Fee, nil
 	} else if util.StringInList(ticker, txn.SupportedAssets()) {
@@ -109,6 +121,7 @@ func (in *SkycoinPendingTransactionInput) GetSpentOutput() core.TransactionOutpu
 }
 
 func (in *SkycoinPendingTransactionInput) GetCoins(ticker string) (uint64, error) {
+	logCoin.Info("Getting coins from " + ticker + " ticker")
 	if util.StringInList(ticker, []string{Sky, CoinHour, CalculatedHour}) {
 		return uint64(0), nil
 	}
@@ -135,6 +148,7 @@ func (sto *SkycoinPendingTransactionOutput) GetAddress() core.Address {
 }
 
 func (sto *SkycoinPendingTransactionOutput) GetCoins(ticker string) (uint64, error) {
+	logCoin.Info("Getting coins from " + ticker + " ticker")
 	accuracy, err := util.AltcoinQuotient(ticker)
 	if err != nil {
 		return uint64(0), err
@@ -198,18 +212,22 @@ type SkycoinUninjectedTransaction struct {
 }
 
 func (skyTxn *SkycoinUninjectedTransaction) SupportedAssets() []string {
+	logCoin.Info("Getting supported assets from un injected transactions")
 	return []string{Sky, CoinHour}
 }
 
 func (skyTxn *SkycoinUninjectedTransaction) GetTimestamp() core.Timestamp {
+	logCoin.Info("Getting timestamp")
 	return 0
 }
 
 func (skyTxn *SkycoinUninjectedTransaction) GetStatus() core.TransactionStatus {
+	logCoin.Info("Getting status for un injected transaction")
 	return core.TXN_STATUS_CREATED
 }
 
 func (skyTxn *SkycoinUninjectedTransaction) GetInputs() []core.TransactionInput {
+	logCoin.Info("Getting inputs from un injected transactions")
 	if skyTxn.inputs == nil {
 		inputs, err := getSkycoinTransactionInputsFromInputsHashes(skyTxn.txn.In)
 		if err != nil {
@@ -221,6 +239,7 @@ func (skyTxn *SkycoinUninjectedTransaction) GetInputs() []core.TransactionInput 
 }
 
 func (skyTxn *SkycoinUninjectedTransaction) GetOutputs() []core.TransactionOutput {
+	logCoin.Info("Getting outputs from un injected transactions")
 	if skyTxn.outputs == nil {
 		outputs := make([]core.TransactionOutput, 0)
 		for _, out := range skyTxn.txn.Out {
@@ -239,6 +258,7 @@ func (skyTxn *SkycoinUninjectedTransaction) GetOutputs() []core.TransactionOutpu
 }
 
 func (skyTxn *SkycoinUninjectedTransaction) ComputeFee(ticker string) (uint64, error) {
+	logCoin.Info("Computing fee for un injected transaction with" + ticker + " ticker")
 	if ticker == CoinHour {
 		return skyTxn.fee, nil
 	}
@@ -246,6 +266,7 @@ func (skyTxn *SkycoinUninjectedTransaction) ComputeFee(ticker string) (uint64, e
 }
 
 func (skyTxn *SkycoinUninjectedTransaction) GetId() string {
+	logCoin.Info("Getting if for un injected transaction")
 	return skyTxn.txn.Hash().String()
 }
 
@@ -261,14 +282,17 @@ type SkycoinTransaction struct {
 }
 
 func (txn *SkycoinTransaction) SupportedAssets() []string {
+	logCoin.Info("Getting supported assets from transactions")
 	return []string{Sky, CoinHour, CalculatedHour}
 }
 
 func (txn *SkycoinTransaction) GetTimestamp() core.Timestamp {
+	logCoin.Info("Getting timestamp transactions")
 	return core.Timestamp(txn.skyTxn.Timestamp)
 }
 
 func (txn *SkycoinTransaction) GetStatus() core.TransactionStatus {
+	logCoin.Info("Getting status for transactions")
 
 	if txn.status == core.TXN_STATUS_CONFIRMED {
 		return txn.status
@@ -289,6 +313,8 @@ func (txn *SkycoinTransaction) GetStatus() core.TransactionStatus {
 }
 
 func (txn *SkycoinTransaction) GetInputs() []core.TransactionInput {
+	logCoin.Info("Getting inputs from transaction")
+
 	if txn.inputs == nil {
 		ins, err := getSkycoinTransactionInputsFromTxnHash(txn.skyTxn.Hash)
 		if err != nil {
@@ -300,6 +326,8 @@ func (txn *SkycoinTransaction) GetInputs() []core.TransactionInput {
 }
 
 func (txn *SkycoinTransaction) GetOutputs() []core.TransactionOutput {
+	logCoin.Info("Getting outputs transactions")
+
 	if txn.outputs == nil {
 		txn.outputs = make([]core.TransactionOutput, 0)
 		for _, out := range txn.skyTxn.Out {
@@ -313,10 +341,12 @@ func (txn *SkycoinTransaction) GetOutputs() []core.TransactionOutput {
 }
 
 func (txn *SkycoinTransaction) GetId() string {
+	logCoin.Info("Getting if from transaction")
 	return txn.skyTxn.Hash
 }
 
 func (txn *SkycoinTransaction) ComputeFee(ticker string) (uint64, error) {
+	logCoin.Info("Compute fee for transaction with " + ticker + "ticker")
 	if ticker == CoinHour {
 		return txn.skyTxn.Fee, nil
 	} else if util.StringInList(ticker, txn.SupportedAssets()) {
@@ -402,10 +432,13 @@ type SkycoinTransactionInput struct {
 }
 
 func (in *SkycoinTransactionInput) GetId() string {
+	logCoin.Info("Getting id for transaction input")
 	return in.skyIn.Hash
 }
 
 func (in *SkycoinTransactionInput) GetSpentOutput() core.TransactionOutput {
+	logCoin.Info("Getting spent outputs for transaction inputs")
+
 	if in.spentOutput == nil {
 
 		c, err := NewSkycoinApiClient(PoolSection)
@@ -435,6 +468,8 @@ func (in *SkycoinTransactionInput) GetSpentOutput() core.TransactionOutput {
 }
 
 func (in *SkycoinTransactionInput) GetCoins(ticker string) (uint64, error) {
+	logCoin.Info("Getting coins for transaction inputs using " + ticker + "ticker")
+
 	accuracy, err2 := util.AltcoinQuotient(ticker)
 	if err2 != nil {
 		return uint64(0), err2
@@ -491,15 +526,18 @@ type SkycoinTransactionOutput struct {
 }
 
 func (out *SkycoinTransactionOutput) GetId() string {
+	logCoin.Info("Getting if of transaction output")
 	return out.skyOut.Hash
 
 }
 
 func (out *SkycoinTransactionOutput) GetAddress() core.Address {
+	logCoin.Info("Getting address for transaction output")
 	return &SkycoinAddress{address: out.skyOut.Address}
 }
 
 func (out *SkycoinTransactionOutput) GetCoins(ticker string) (uint64, error) {
+	logCoin.Info("Getting coins for transaction outputs using " + ticker + "ticker")
 	accuracy, err2 := util.AltcoinQuotient(ticker)
 	if err2 != nil {
 		return uint64(0), err2
@@ -519,6 +557,7 @@ func (out *SkycoinTransactionOutput) GetCoins(ticker string) (uint64, error) {
 }
 
 func (out *SkycoinTransactionOutput) IsSpent() bool {
+	logCoin.Info("Checking if output is spent")
 	if out.spent {
 		return true
 	}
