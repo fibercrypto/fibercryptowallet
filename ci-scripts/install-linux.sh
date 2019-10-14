c117a7712ee2f8a880587207a7f6fdbe0dafe6d0
Alvaro Denis <dev0003@simelo.tech>
2019-10-14 18:14:36 -0400

    install pip in travis ref #139

--- ci-scripts/install-linux.sh
+++ ci-scripts/install-linux.sh
@@ -5,5 +5,7 @@ set -x
 sudo apt-get update
 # SDL for building hardware wallet emulator
 sudo apt install libsdl2-dev libsdl2-image-dev libegl1-mesa-dev libgles2-mesa-dev
+# PIP
+sudo apt-get install python3-pip python-pip
 # OpenGL swrast driver for xvfb
 sudo apt install mesa-utils and libgl1-mesa-glx
