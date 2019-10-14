725377afb5adf94b9270816ecf2b0481a7ae568c
Alvaro Denis <dev0003@simelo.tech>
2019-10-14 17:55:11 -0400

    install protobf in travis ref #139

---
+++ ci-scripts/install-linux.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -x
+
+sudo apt-get update
+# SDL for building hardware wallet emulator
+sudo apt install libsdl2-dev libsdl2-image-dev libegl1-mesa-dev libgles2-mesa-dev
+# OpenGL swrast driver for xvfb
+sudo apt install mesa-utils and libgl1-mesa-glx
