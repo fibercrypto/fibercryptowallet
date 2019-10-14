513e7bb4785f5d597ff4c92e711752b6769034cc
Alvaro Denis <dev0003@simelo.tech>
2019-10-14 16:28:38 -0400

    Add TestGetSignerDescriptionShouldBeOk ref #139

--- src/hardware/sky-wallet_test.go
+++ src/hardware/sky-wallet_test.go
@@ -93,3 +93,26 @@ func TestGetSignerUIDShouldFailForInvalidMessageType(t *testing.T) {
 	// Then
 	require.Equal(t, core.UID("undefined"), devId)
 }
+
+func TestGetSignerDescriptionShouldBeOk(t *testing.T) {
+	// Giving
+	dev := mocks.Devicer{}
+	expectedDevDescription := urnPrefix+"c24a046d-7d7b-484d-baf6-abedd883023f"
+	f := messages.Features{
+		Label: proto.String("c24a046d-7d7b-484d-baf6-abedd883023f"),
+	}
+	fb, err := proto.Marshal(&f)
+	require.Nil(t, err)
+	msg := wire.Message{
+		Kind: uint16(messages.MessageType_MessageType_Features),
+		Data: fb,
+	}
+	dev.On("GetFeatures").Return(msg, nil)
+	sw := NewSkyWallet(&dev)
+
+	// When
+	devDescription := sw.GetSignerDescription()
+
+	// Then
+	require.Equal(t, expectedDevDescription, devDescription)
+}
