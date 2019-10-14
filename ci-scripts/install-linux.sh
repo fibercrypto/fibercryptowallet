3fdbe19146f1402fca5a779b471c84e2da207ef6
Alvaro Denis <dev0003@simelo.tech>
2019-10-14 19:34:23 -0400

    use python3 to install proto deps and track mocks ref #139

--- ci-scripts/install-linux.sh
+++ ci-scripts/install-linux.sh
@@ -6,8 +6,6 @@ sudo apt-get update
 # SDL for building hardware wallet emulator
 sudo apt install libsdl2-dev libsdl2-image-dev libegl1-mesa-dev libgles2-mesa-dev
 # PIP
-sudo apt-get install python3-pip python-pip
-# python protobuf
-sudo apt-get install python-protobuf
+sudo apt-get install python3-pip
 # OpenGL swrast driver for xvfb
 sudo apt install mesa-utils and libgl1-mesa-glx
