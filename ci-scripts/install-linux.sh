533b214454602dcbfa43fa547f848bb7698a79ff
Alvaro Denis <dev0003@simelo.tech>
2019-10-14 18:49:36 -0400

    install protobuf from pip in travis ref #139

--- ci-scripts/install-linux.sh
+++ ci-scripts/install-linux.sh
@@ -7,7 +7,5 @@ sudo apt-get update
 sudo apt install libsdl2-dev libsdl2-image-dev libegl1-mesa-dev libgles2-mesa-dev
 # PIP
 sudo apt-get install python3-pip python-pip
-# python protobuf
-sudo apt-get install python-protobuf
 # OpenGL swrast driver for xvfb
 sudo apt install mesa-utils and libgl1-mesa-glx
