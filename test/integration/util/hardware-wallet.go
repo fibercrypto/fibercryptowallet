45fb344ad3d159f3739af4885cc789d139b3cc6a
Alvaro Denis <dev0003@simelo.tech>
2019-10-26 20:18:18 -0400

    create some util function for device interaction in tests ref #139

---
+++ test/integration/util/hardware-wallet.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"github.com/skycoin/hardware-wallet-go/src/skywallet"
+	"github.com/skycoin/hardware-wallet-go/src/skywallet/wire"
+	messages "github.com/skycoin/hardware-wallet-protob/go"
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+func forceWipe(t *testing.T, dev *skywallet.Device) {
+	err := dev.SetAutoPressButton(true, skywallet.ButtonRight)
+	require.NoError(t, err)
+	msg, err := dev.Wipe()
+	require.NoError(t, err)
+	PressAcceptButton(t, dev, msg, messages.MessageType_MessageType_Success)
+}
+
+func PressAcceptButton(t *testing.T, dev skywallet.Devicer, prvMsg wire.Message, nextMsg messages.MessageType) wire.Message {
+	require.Equal(t, uint16(messages.MessageType_MessageType_ButtonRequest), prvMsg.Kind)
+	msg, err := dev.ButtonAck()
+	require.NoError(t, err)
+	require.Equal(t, uint16(nextMsg), msg.Kind)
+	if msg.Kind == uint16(messages.MessageType_MessageType_Success) {
+		successMsg, err := skywallet.DecodeSuccessMsg(msg)
+		require.NoError(t, err)
+		require.NotNil(t, successMsg)
+	}
+	return msg
+}
+
+func ForceSetMnemonic(t *testing.T, dev *skywallet.Device, mnemonic string) {
+	forceWipe(t, dev)
+	msg, err := dev.SetMnemonic(mnemonic)
+	require.NoError(t, err)
+	PressAcceptButton(t, dev, msg, messages.MessageType_MessageType_Success)
+}
