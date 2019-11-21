465064553a00a91c12b83882a729a6e7b31bdcef
Olemis Lang <olemis@simelo.tech>
2019-11-20 19:56:33 -0500

    [util] refs #234 - Assertions for signer description in TestSignerMethods

--- src/util/sign_test.go
+++ src/util/sign_test.go
@@ -5,13 +5,15 @@ import (
 
 	"github.com/fibercrypto/FiberCryptoWallet/src/coin/mocks"
 	"github.com/fibercrypto/FiberCryptoWallet/src/core"
+	"github.com/skycoin/skycoin/src/testutil"
 	"github.com/stretchr/testify/require"
 )
 
-func TestAddLookupSigner(t *testing.T) {
+func TestSignerMethods(t *testing.T) {
 	signerID1 := core.UID("TestAddLookupSigner#1")
 	signer1 := new(mocks.TxnSigner)
 	signer1.On("GetSignerUID").Return(signerID1)
+	signer1.On("GetSignerDescription").Return(string(signerID1))
 	require.Equal(t, signer1.GetSignerUID(), signerID1)
 	err := AttachSignService(signer1)
 	require.NoError(t, err)
@@ -19,4 +21,13 @@ func TestAddLookupSigner(t *testing.T) {
 	signer := LookupSignService(signerID1)
 	require.NotNil(t, signer)
 	require.Equal(t, signer, signer1)
+	desc, err := GetSignerDescription(signerID1)
+	require.NoError(t, err)
+	require.Equal(t, string(signerID1), desc)
+
+	err = RemoveSignService(signerID1)
+	signer = LookupSignService(signerID1)
+	require.Nil(t, signer)
+	desc, err = GetSignerDescription(signerID1)
+	testutil.RequireError(t, err, "Invalid Id")
 }
