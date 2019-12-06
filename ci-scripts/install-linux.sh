14dbb0545aa23e5055ed0345dbb04e1a780e2f69
Alvaro Denis <dev0003@simelo.tech>
2019-12-06 12:29:55 -0500

    move some instalation rules from travis.yml to ci-scripts ref #139

--- ci-scripts/install-linux.sh
+++ ci-scripts/install-linux.sh
@@ -3,9 +3,13 @@
 set -x
 
 sudo apt-get update
-# SDL for building hardware wallet emulator
-sudo apt install libsdl2-dev libsdl2-image-dev libegl1-mesa-dev libgles2-mesa-dev
 # PIP
 sudo apt-get install python3-pip
 # OpenGL swrast driver for xvfb
 sudo apt install mesa-utils and libgl1-mesa-glx
+# Hardware wallet for running tests against emulator
+mkdir -p tmp/hardware-wallet
+git clone --depth=1 --single-branch --branch develop https://github.com/skycoin/hardware-wallet.git tmp/hardware-wallet
+git -C tmp/hardware-wallet submodule update --init --recursive
+( cd ./tmp/hardware-wallet && sh "ci-scripts/install-linux.sh" )
+export PATH="/usr/local/bin:$(pwd)/tmp/hardware-wallet/protoc/bin:$PATH"
--- ci-scripts/install-osx.sh
+++ ci-scripts/install-osx.sh
@@ -10,5 +10,10 @@ brew install sdl2_image sdl2 mesa mesalib-glw
 brew update
 # USB HID enumeration libraries
 brew install libusb
-# SDL for building hardware wallet emulator
-brew install sdl2_image sdl2 mesa mesalib-glw
+# Install OS-specific test and build dependencies for hardware-wallet
+mkdir -p tmp/hardware-wallet
+git clone --depth=1 --single-branch --branch develop https://github.com/skycoin/hardware-wallet.git tmp/hardware-wallet
+git -C tmp/hardware-wallet submodule update --init --recursive
+( cd ./tmp/hardware-wallet && sh "ci-scripts/install-osx.sh" )
+# Include paths for brew packages e.g. SDL2
+export SDL_INCLUDE="$(brew --prefix sdl2)/include/SDL2";
