d2954b500065f24607934ecc8245cb3be75c1afc
Alvaro Denis <dev0003@simelo.tech>
2019-12-17 21:13:17 -0500

    fix tests with new Devicer implementation ref #139

--- src/contrib/skywallet/skywallet_integration_test.go
+++ src/contrib/skywallet/skywallet_integration_test.go
@@ -6,9 +6,8 @@ import (
 	"github.com/fibercrypto/fibercryptowallet/src/util"
 	"github.com/fibercrypto/fibercryptowallet/src/util/logging"
 	integrationtestutil "github.com/fibercrypto/fibercryptowallet/test/integration/util"
+	"github.com/fibercrypto/skywallet-go/src/integration/proxy"
 	"github.com/fibercrypto/skywallet-go/src/skywallet"
-	"github.com/fibercrypto/skywallet-go/src/skywallet/wire"
-	messages "github.com/fibercrypto/skywallet-protob/go"
 	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
@@ -16,7 +15,7 @@ import (
 
 func setUpHardwareWallet(t *testing.T) {
 	util.RegisterAltcoin(skycoin.NewSkyFiberPlugin(skycoin.SkycoinMainNetParams))
-	dev := skywallet.NewDevice(skywallet.DeviceTypeEmulator)
+	dev := proxy.NewSequencer(skywallet.NewDevice(skywallet.DeviceTypeEmulator))
 	keyTestData, err := skycoin.GenerateTestKeyPair(t)
 	require.NoError(t, err)
 	integrationtestutil.ForceSetMnemonic(t, dev, keyTestData.Mnemonic)
@@ -40,23 +39,10 @@ func TestMain(m *testing.M) {
 func TestTransactionSignInputFromDevice(t *testing.T) {
 	skycoin.CleanGlobalMock()
 	setUpHardwareWallet(t)
-	callback := func(dev skywallet.Devicer, prvMsg wire.Message, outsLen int) (wire.Message, error) {
-		var msg wire.Message
-		for outsLen > 0 {
-			integrationtestutil.PressAcceptButton(t, dev, prvMsg, messages.MessageType_MessageType_ButtonRequest)
-			if outsLen == 1 {
-				msg = integrationtestutil.PressAcceptButton(t, dev, prvMsg, messages.MessageType_MessageType_ResponseTransactionSign)
-			} else {
-				msg = integrationtestutil.PressAcceptButton(t, dev, prvMsg, messages.MessageType_MessageType_ButtonRequest)
-			}
-			outsLen--
-		}
-		return msg, nil
-	}
-	dev := skywallet.NewDevice(skywallet.DeviceTypeEmulator)
-	hs1 := NewSkyWallet(nil, dev, callback)
-	hs2 := NewSkyWallet(nil, dev, callback)
-	hs3 := NewSkyWallet(nil, dev, callback)
+	dev := proxy.NewSequencer(skywallet.NewDevice(skywallet.DeviceTypeEmulator))
+	hs1 := NewSkyWallet(nil, dev)
+	hs2 := NewSkyWallet(nil, dev)
+	hs3 := NewSkyWallet(nil, dev)
 	setWallet := func(t *testing.T, signer core.TxnSigner, wlt core.Wallet) {
 		hs, ok := signer.(*SkyWallet)
 		require.True(t, ok)
--- test/integration/util/hardware-wallet.go
+++ test/integration/util/hardware-wallet.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func forceWipe(t *testing.T, dev *skywallet.Device) {
+func forceWipe(t *testing.T, dev skywallet.Devicer) {
 	err := dev.SetAutoPressButton(true, skywallet.ButtonRight)
 	require.NoError(t, err)
 	msg, err := dev.Wipe()
@@ -29,7 +29,7 @@ func PressAcceptButton(t *testing.T, dev skywallet.Devicer, prvMsg wire.Message,
 	return msg
 }
 
-func ForceSetMnemonic(t *testing.T, dev *skywallet.Device, mnemonic string) {
+func ForceSetMnemonic(t *testing.T, dev skywallet.Devicer, mnemonic string) {
 	forceWipe(t, dev)
 	msg, err := dev.SetMnemonic(mnemonic)
 	require.NoError(t, err)
