dcb720087e15df3d0b8caeed5c8df336b645f725
Marcos Maceo <marcosmaceowork@gmail.com>
2019-12-19 19:20:32 -0500

    [ci][win] refs #135 add travis_wait to qt installation

--- .travis/install-qt-win.sh
+++ .travis/install-qt-win.sh
@@ -3,3 +3,4 @@ wget "http://download.qt.io/official_releases/online_installers/qt-unified-windo
 export MWDIR="/c/Users/travis/Qt/Tools/mingw730_64"
 export QTDIR="/c/Users/travis/Qt/5.12.3/mingw73_64"
 export PATH="$MWDIR/bin:$QTDIR/bin:$PATH"
+echo "QT already installed"
