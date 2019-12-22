cb65099206ccfcfad65eb780fecf0b365e32e303
Mauricio Perdomo <ml.perdomo97@gmail.com>
2019-12-21 21:18:55 -0500

    Use script for avoid travis timeout

--- .travis/install-qt-win.sh
+++ .travis/install-qt-win.sh
@@ -1,4 +1,22 @@
-curl -vLo ~/qt-unified-windows-x86-online.exe http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe
-if ! ~/qt-unified-windows-x86-online.exe --verbose --script .travis/qt-installer-windows.qs > ~/qt-installer-output.txt; then
-  cat ~/qt-installer-output.txt; exit 1
+set -eu -o pipefail
+
+if [ -e "$QT5_BASE_DIR/bin/moc.exe" ]; then
+    echo "Found an existing Qt installation at $QT5_BASE_DIR"
+    exit
 fi
+
+echo "Downloading the installer..."
+# https is of no use if it redirects to a http mirror...
+curl -vLo ~/qt-unified-windows-x86-online.exe http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe
+
+echo "Installing..."
+# Run installer and save the installer output. To avoid hitting the timeout,
+# periodically print some progress. On error, show the full log and abort.
+~/qt-unified-windows-x86-online.exe --verbose --script .travis/qt-installer-windows.qs |
+    tee ~/qt-installer-output.txt |
+    tools/report-progress.sh ||
+    (cat ~/qt-installer-output.txt; exit 1)
+
+printf 'Installation size: '
+du -sm "$QT5_BASE_DIR" 2>&1 ||
+    (cat ~/qt-installer-output.txt; exit 1)
