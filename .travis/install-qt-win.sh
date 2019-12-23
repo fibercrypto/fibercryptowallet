708116cfbcd719d9fc89cd3962607a93c963b4ae
Marcos Maceo <marcosmaceowork@gmail.com>
2019-12-23 12:31:31 -0500

    [ci] refs #135 Add Appveyor to project

--- .travis/install-qt-win.sh
+++ .travis/install-qt-win.sh
@@ -1,6 +1,23 @@
-wget "http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe" -O qt.exe
-./qt.exe --verbose --script util/qt-headless.qs					
-export MWDIR="/c/Users/travis/Qt/Tools/mingw730_64"
-export QTDIR="/c/Users/travis/Qt/5.12.3/mingw73_64"
-export PATH="$MWDIR/bin:$QTDIR/bin:$PATH"
-echo "QT already installed"
+set -eu -o pipefail
+
+if [ -e "$QT5_BASE_DIR/bin/moc.exe" ]; then
+    echo "Found an existing Qt installation at $QT5_BASE_DIR"
+    exit
+fi
+
+echo "Downloading the installer..."
+# https is of no use if it redirects to a http mirror...
+curl -vLo ~/qt-unified-windows-x86-online.exe http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe
+
+echo "Installing..."
+# Run installer and save the installer output. To avoid hitting the timeout,
+# periodically print some progress. On error, show the full log and abort.
+~/qt-unified-windows-x86-online.exe --verbose --script .travis/qt-installer-windows.qs 
+#|
+#    tee ~/qt-installer-output.txt |
+#    .travis/report-progress.sh ||
+#    (cat ~/qt-installer-output.txt; exit 1)
+#
+#printf 'Installation size: '
+#du -sm "$QT5_BASE_DIR" 2>&1 ||
+#    (cat ~/qt-installer-output.txt; exit 1)
