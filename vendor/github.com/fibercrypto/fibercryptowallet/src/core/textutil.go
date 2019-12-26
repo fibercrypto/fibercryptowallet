dd928dfc75b0f3ae15d1211ace64f67180306b34
Marcos Maceo <marcosmaceowork@gmail.com>
2019-12-26 23:36:14 +0100

    Update vendor

--- vendor/github.com/fibercrypto/fibercryptowallet/src/core/textutil.go
+++
@@ -1,4 +0,0 @@
-package core
-
-// PasswordReader secure retrieval of passwords from users
-type PasswordReader func(string, KeyValueStore) (string, error)
--- vendor/github.com/fibercrypto/fibercryptowallet/src/models/pending/Pending.go
+++
@@ -1,6 +0,0 @@
-package pending
-
-func init() {
-	PendingTransactionList_QmlRegisterType2("PendingModels", 1, 0, "QPendingList")
-	PendingTransaction_QmlRegisterType2("PendingModels", 1, 0, "QPendingTransaction")
-}
