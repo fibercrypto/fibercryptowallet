d9c840bac14d4ca2d9d21627468f9e0892a278ef
Raul Iglesias <riglesiasvera@gmail.com>
2019-12-29 19:20:21 -0500

    [test] refs #279 - Add initial test for NewGenericOutput
    Pending:
    -  Find how to register skycoin coins without import the skycoin package, import it produces cyclic imports

---
+++ src/util/coin_test.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/fibercrypto/fibercryptowallet/src/coin/mocks"
+)
+
+func TestNewGenericOutput(t *testing.T) {
+	addr := &mocks.Address{}
+
+	output := NewGenericOutput(addr, "id")
+	require.Equal(t, GenericOutput{Address: addr, id: "id", Balance: make(map[string]uint64)}, output)
+
+	output.SetCoins("some_coin", uint64(20))
+	some_coin, err := output.GetCoins("some_coin")
+	require.Equal(t, uint64(20), some_coin)
+	require.NoError(t, err)
+
+	require.Equal(t, addr, output.GetAddress())
+	require.Equal(t, "id", output.GetId())
+	require.False(t, output.IsSpent())
+
+	assets := output.SupportedAssets()
+	require.Equal(t, 1, len(assets))
+	find := false
+	for _, asset := range assets {
+		if asset == "some_coin" {
+			find = true
+			break
+		}
+	}
+	require.True(t, find)
+
+	//TODO: Find how to test PushCoin. There are problems registering coins
+
+}
