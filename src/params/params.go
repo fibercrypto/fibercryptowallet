c6ff73a882eb4ec6a5d6e1aae170cfb2a3e378f9
Marcos Maceo <marcosmaceowork@gmail.com>
2020-01-09 14:31:13 -0500

    [params] refs #295 Add params for general settings app

--- src/params/params.go
+++ src/params/params.go
@@ -3,4 +3,9 @@ package params
 const (
 	// Default refresh timeout for API data
 	DataRefreshTimeout = 1000000 //FIXME: set correct value
+	DataUpdateTime	   = 5000000
+	OrganizationName   = "Simelo.Tech"
+	OrganizationDomain = "simelo.tech"
+	ApplicationName    = "FiberCryptoWallet"
+	ApplicationVersion = "0.27.0"
 )
