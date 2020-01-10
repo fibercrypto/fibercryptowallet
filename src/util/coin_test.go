5471baace191e933c2dca4a35fc3516b58f0fd74
Lazaro Raul <riglesiasvera@gmail.com>
2020-01-09 22:20:13 -0500

    [test] refs #279 - Improve TestNewGenericOutput

--- src/util/coin_test.go
+++ src/util/coin_test.go
@@ -1,42 +1,61 @@
 package util
 
 import (
+	"sort"
 	"testing"
 
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/fibercrypto/fibercryptowallet/src/coin/mocks"
+	"github.com/fibercrypto/fibercryptowallet/src/core"
 )
 
 func TestNewGenericOutput(t *testing.T) {
-	addr := &mocks.Address{}
+	fakeTicker := "MOCKSCOIN"
+	fakeDesc := "Fake coin"
+	fakeExp := 3
+	fakeMeta := core.AltcoinMetadata{
+		Name:          fakeDesc,
+		Ticker:        fakeTicker,
+		Family:        fakeTicker,
+		HasBip44:      false,
+		Bip44CoinType: 0,
+		Accuracy:      int32(fakeExp),
+	}
+	mockPlugin := new(mocks.AltcoinPlugin)
+	mockPlugin.On("RegisterTo", mock.Anything).Return().Run(func(args mock.Arguments) {
+		manager := args.Get(0).(core.AltcoinManager)
+		manager.RegisterAltcoin(fakeMeta, mockPlugin)
+	})
+	mockPlugin.On("GetName").Return(fakeDesc)
+	RegisterAltcoin(mockPlugin)
 
+	addr := new(mocks.Address)
+
+	invalid_coin := "some_coin"
 	output := NewGenericOutput(addr, "id")
 	require.Equal(t, GenericOutput{Address: addr, id: "id", Balance: make(map[string]uint64)}, output)
 
-	output.SetCoins("some_coin", uint64(20))
-	some_coin, err := output.GetCoins("some_coin")
+	output.PushCoins(fakeTicker, "42")
+	output.SetCoins(invalid_coin, uint64(20))
+	some_coin, err := output.GetCoins(invalid_coin)
 	require.Equal(t, uint64(20), some_coin)
-	require.NoError(t, err)
+	require.Nil(t, err)
+	some_coin, err = output.GetCoins(fakeTicker)
+	require.Equal(t, uint64(42000), some_coin)
+	require.Nil(t, err)
 
 	require.Equal(t, addr, output.GetAddress())
 	require.Equal(t, "id", output.GetId())
 	require.False(t, output.IsSpent())
 
+	testAssets := []string{fakeTicker, invalid_coin}
+	sort.Strins(testAssets)
 	assets := output.SupportedAssets()
-	require.Equal(t, 1, len(assets))
-	find := false
-	for _, asset := range assets {
-		if asset == "some_coin" {
-			find = true
-			break
-		}
-	}
-	require.True(t, find)
+	sort.Strins(assets)
+	require.Equal(t, testAssets, assets)
 
-	_, err2 := output.GetCoins("invalid_coin")
+	_, err2 := output.GetCoins("other_coin")
 	require.Error(t, err2)
-
-	//TODO: Find how to test PushCoin. There are problems registering coins
-
 }
