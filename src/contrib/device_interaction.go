4b5db43c5f146b797fc616d67a05da1f870ea296
Alvaro Denis <dev0003@simelo.tech>
2020-01-09 20:31:54 -0500

    Add DeviceInteraction intrface
    Add a wrapper for the Devicer implementation to have an async alternative
    by using github.com/chebyrash/promise
    ref #255

---
+++ src/contrib/device_interaction.go
@@ -0,0 +1,24 @@
+package contrib
+
+import (
+	"github.com/chebyrash/promise"
+	messages "github.com/fibercrypto/skywallet-protob/go"
+)
+
+type DeviceInteraction interface {
+	AddressGen(addressN, startIndex uint32, confirmAddress bool, walletType string) *promise.Promise
+	ApplySettings(usePassphrase *bool, label string, language string) *promise.Promise
+	Backup() *promise.Promise
+	CheckMessageSignature(message, signature, address string) *promise.Promise
+	ChangePin(removePin *bool) *promise.Promise
+	IsAvailable() *promise.Promise
+	FirmwareUpload(payload []byte, hash [32]byte) *promise.Promise
+	Features() *promise.Promise
+	GenerateMnemonic(wordCount uint32, usePassphrase bool) *promise.Promise
+	Recovery(wordCount uint32, usePassphrase *bool, dryRun bool) *promise.Promise
+	// FIXME: change inputs and outputs to the generic types:
+	// core.TransactionInputIterator and core.TransactionOutputIterator respectively
+	TransactionSign(inputs []*messages.SkycoinTransactionInput, outputs []*messages.SkycoinTransactionOutput, walletType string) *promise.Promise
+	SignMessage(addressN, addressIndex int, message string, walletType string) *promise.Promise
+	Wipe() *promise.Promise
+}
---
+++ src/contrib/skywallet/skywallet_device_interaction.go
@@ -0,0 +1,204 @@
+package hardware
+
+import (
+	"github.com/chebyrash/promise"
+	"github.com/fibercrypto/skywallet-go/src/skywallet"
+	messages "github.com/fibercrypto/skywallet-protob/go"
+	"github.com/gogo/protobuf/proto"
+)
+
+type SkyWalletInteraction struct {
+	dev skywallet.Devicer
+}
+
+func(wlt *SkyWalletInteraction) AddressGen(addressN, startIndex uint32, confirmAddress bool, walletType string) *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.AddressGen(addressN, startIndex, confirmAddress, walletType)
+		if err != nil {
+			reject(err)
+			return
+		}
+		addresses, err := skywallet.DecodeResponseSkycoinAddress(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(addresses)
+	})
+}
+
+func(wlt *SkyWalletInteraction) ApplySettings(usePassphrase *bool, label string, language string) *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.ApplySettings(usePassphrase, label, language)
+		if err != nil {
+			reject(err)
+			return
+		}
+		msgStr, err := skywallet.DecodeSuccessMsg(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(msgStr)
+	})
+}
+
+func(wlt *SkyWalletInteraction) Backup() *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.Backup()
+		if err != nil {
+			reject(err)
+			return
+		}
+		msgStr, err := skywallet.DecodeSuccessMsg(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(msgStr)
+	})
+}
+
+func(wlt *SkyWalletInteraction) CheckMessageSignature(message, signature, address string) *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.CheckMessageSignature(message, signature, address)
+		if err != nil {
+			reject(err)
+			return
+		}
+		msgStr, err := skywallet.DecodeSuccessMsg(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(msgStr)
+	})
+}
+
+func(wlt *SkyWalletInteraction) ChangePin(removePin *bool) *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.ChangePin(removePin)
+		if err != nil {
+			reject(err)
+			return
+		}
+		msgStr, err := skywallet.DecodeSuccessMsg(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(msgStr)
+	})
+}
+
+func(wlt *SkyWalletInteraction) IsAvailable() *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		resolve(wlt.dev.Available())
+	})
+}
+
+func(wlt *SkyWalletInteraction) FirmwareUpload(payload []byte, hash [32]byte) *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		if err := wlt.dev.FirmwareUpload(payload, hash); err != nil {
+			reject(err)
+			return
+		}
+		resolve(nil)
+	})
+}
+
+func(wlt *SkyWalletInteraction) Features() *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.GetFeatures()
+		if err != nil {
+			reject(err)
+			return
+		}
+		features := messages.Features{}
+		if err = proto.Unmarshal(msg.Data, &features); err != nil {
+			reject(err)
+			return
+		}
+		resolve(features)
+	})
+}
+
+func(wlt *SkyWalletInteraction) GenerateMnemonic(wordCount uint32, usePassphrase bool) *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.GenerateMnemonic(wordCount, usePassphrase)
+		if err != nil {
+			reject(err)
+			return
+		}
+		msgStr, err := skywallet.DecodeSuccessMsg(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(msgStr)
+	})
+}
+
+func(wlt *SkyWalletInteraction) Recovery(wordCount uint32, usePassphrase *bool, dryRun bool) *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.Recovery(wordCount, usePassphrase, dryRun)
+		if err != nil {
+			reject(err)
+			return
+		}
+		msgStr, err := skywallet.DecodeSuccessMsg(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(msgStr)
+	})
+}
+
+func(wlt *SkyWalletInteraction) TransactionSign(inputs []*messages.SkycoinTransactionInput, outputs []*messages.SkycoinTransactionOutput, walletType string) *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.TransactionSign(inputs, outputs, walletType)
+		if err != nil {
+			reject(err)
+			return
+		}
+		signatures, err := skywallet.DecodeResponseTransactionSign(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(signatures)
+	})
+}
+
+func(wlt *SkyWalletInteraction) SignMessage(addressN, addressIndex int, message string, walletType string) *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.SignMessage(addressN, addressIndex, message, walletType)
+		if err != nil {
+			reject(err)
+			return
+		}
+		msgStr, err := skywallet.DecodeResponseSkycoinSignMessage(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(msgStr)
+	})
+}
+
+func(wlt *SkyWalletInteraction) Wipe() *promise.Promise {
+	return promise.New(func(resolve func(interface{}), reject func(error)) {
+		msg, err := wlt.dev.Wipe()
+		if err != nil {
+			reject(err)
+			return
+		}
+		msgStr, err := skywallet.DecodeSuccessMsg(msg)
+		if err != nil {
+			reject(err)
+			return
+		}
+		resolve(msgStr)
+	})
+}
