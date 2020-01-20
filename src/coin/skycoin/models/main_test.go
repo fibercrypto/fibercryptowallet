48fdd09ad99b9f4e55d5411257a0af2a4df52717
Lazaro Raul <riglesiasvera@gmail.com>
2020-01-20 17:49:04 -0500

    [test] refs #279 - Add `TestSkyFiberPluginRegisterTo` and `TestSkyFiberPluginFunctions`

--- src/coin/skycoin/models/main_test.go
+++ src/coin/skycoin/models/main_test.go
@@ -1,9 +1,14 @@
 package skycoin
 
 import (
-	"github.com/fibercrypto/fibercryptowallet/src/util/logging"
 	"os"
+	"sort"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/fibercrypto/fibercryptowallet/src/coin/mocks"
+	"github.com/fibercrypto/fibercryptowallet/src/util/logging"
 
 	"github.com/fibercrypto/fibercryptowallet/src/core"
 	util "github.com/fibercrypto/fibercryptowallet/src/util"
@@ -33,4 +38,77 @@ func TestMain(m *testing.M) {
 	}
 	util.RegisterAltcoin(NewSkyFiberPlugin(SkycoinMainNetParams))
 	os.Exit(m.Run())
+}
+
+func TestSkyFiberPluginFunctions(t *testing.T) {
+	name := "SkyFiber"
+	family := []string{SkycoinFamily}
+	description := "FiberCrypto wallet connector for Skycoin and SkyFiber altcoins"
+	altcoins := []core.AltcoinMetadata{
+		core.AltcoinMetadata{
+			Name:     SkycoinName,
+			Ticker:   SkycoinTicker,
+			Family:   SkycoinFamily,
+			HasBip44: false,
+			Accuracy: 6,
+		},
+		core.AltcoinMetadata{
+			Name:     CoinHoursName,
+			Ticker:   CoinHoursTicker,
+			Family:   SkycoinFamily,
+			HasBip44: false,
+			Accuracy: 0,
+		},
+		core.AltcoinMetadata{
+			Name:     CalculatedHoursName,
+			Ticker:   CalculatedHoursTicker,
+			Family:   SkycoinFamily,
+			HasBip44: false,
+			Accuracy: 0,
+		},
+	}
+	cmp := func(altcoinList []core.AltcoinMetadata) {
+		sort.SliceStable(altcoinList, func(i, j int) bool {
+			return altcoinList[i].Name < altcoinList[j].Name
+		})
+	}
+
+	plugin := NewSkyFiberPlugin(SkycoinMainNetParams)
+	require.NotNil(t, plugin)
+	require.Equal(t, name, plugin.GetName())
+	require.Equal(t, description, plugin.GetDescription())
+
+	pluginFamily := plugin.ListSupportedFamilies()
+	sort.Strings(pluginFamily)
+	sort.Strings(family)
+	require.Equal(t, family, pluginFamily)
+
+	pluginAltcoins := plugin.ListSupportedAltcoins()
+	cmp(pluginAltcoins)
+	cmp(altcoins)
+	require.Equal(t, altcoins, pluginAltcoins)
+}
+
+func TestSkyFiberPluginRegisterTo(t *testing.T) {
+	plugin := NewSkyFiberPlugin(SkycoinMainNetParams)
+	altcoins := plugin.ListSupportedAltcoins()
+
+	manager := new(mocks.AltcoinManager)
+	manager.On("RegisterAltcoin", mock.AnythingOfType("core.AltcoinMetadata"), plugin).Return().Run(
+		func(args mock.Arguments) {
+			meta := args.Get(0).(core.AltcoinMetadata)
+			idx := -1
+			for i, altcoin := range altcoins {
+				if altcoin == meta {
+					idx = i
+					break
+				}
+			}
+			require.NotEqual(t, -1, idx)
+			altcoins[idx] = altcoins[len(altcoins)-1]
+			altcoins = altcoins[:len(altcoins)-1]
+		},
+	)
+	plugin.RegisterTo(manager)
+	require.Equal(t, 0, len(altcoins))
 }
