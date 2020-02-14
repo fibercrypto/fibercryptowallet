5be3a3839c76318af64da84b6c460def76bec461
Alvaro Denis <dev0003@simelo.tech>
2020-02-13 22:53:31 -0500

    fix: be able to use the wallet it selft on having multiple signers ref #139

--- src/models/walletsManager.go
+++ src/models/walletsManager.go
@@ -618,6 +618,10 @@ func (walletM *WalletManager) signTxn(wltIds, address []string, source string, t
 			logWalletManager.WithError(err).Warnf("No signer %s for wallet %v", source, wlts[0])
 			return nil
 		}
+		if suid, err := signer.GetSignerUID(); err != nil && wlts[0].GetId() == string(suid) {
+			// NOTE the signer is the wallet it self
+			signer = nil
+		}
 		txn, err = wlts[0].Sign(qTxn.txn, signer, pwd, nil)
 	}
 
