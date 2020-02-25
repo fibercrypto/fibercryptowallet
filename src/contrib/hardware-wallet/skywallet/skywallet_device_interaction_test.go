3044b2546edf869f620d2c38d8d72a50ee1b085c
Alvaro Denis <dev0003@simelo.tech>
2020-02-25 17:17:25 -0500

    add unit tests for device interacion SignMessage ref #255

--- src/contrib/hardware-wallet/skywallet/skywallet_device_interaction_test.go
+++ src/contrib/hardware-wallet/skywallet/skywallet_device_interaction_test.go
@@ -766,3 +766,68 @@ func TestTransactionSignShouldHandleErrorDecodingResponse(t *testing.T) {
 	require.Error(t, err)
 	require.Equal(t, err, errors.New("calling DecodeResponseeSkycoinSignMessage with wrong message type: MessageType_Failure"))
 }
+
+func TestSignMessageShouldWorkOk(t *testing.T) {
+	// Giving
+	dev := &mocks.Devicer{}
+	orgMsg := "great"
+	signMessageResp := messages.ResponseSkycoinSignMessage{SignedMessage: proto.String(orgMsg)}
+	data, err := proto.Marshal(&signMessageResp)
+	require.NoError(t, err)
+	msg := wire.Message{
+		Kind: uint16(messages.MessageType_MessageType_ResponseSkycoinSignMessage),
+		Data: data,
+	}
+	dev.On("SignMessage", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(msg, nil)
+	di := createDeviceInteraction2(dev)
+
+	// When
+	response, err := di.SignMessage(1, 1, "", "").Then(func(data interface{}) interface{} {
+		return data
+	}).Await()
+
+	// Then
+	require.NoError(t, err)
+	responseStr, ok := response.(string)
+	require.True(t, ok)
+	require.Equal(t, orgMsg, responseStr)
+}
+
+func TestSignMessageShouldHandleDeviceError(t *testing.T) {
+	// Giving
+	dev := &mocks.Devicer{}
+	dev.On("SignMessage", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(wire.Message{}, errors.New(""))
+	di := createDeviceInteraction2(dev)
+
+	// When
+	_, err := di.SignMessage(1, 1, "", "").Then(func(data interface{}) interface{} {
+		return data
+	}).Await()
+
+	// Then
+	require.Error(t, err)
+}
+
+func TestSignMessageShouldHandleErrorDecodingResponse(t *testing.T) {
+	// Giving
+	dev := &mocks.Devicer{}
+	orgResp := "jhfjdhfjd"
+	signMessageResp := messages.Failure{Message: proto.String(orgResp)}
+	data, err := proto.Marshal(&signMessageResp)
+	require.NoError(t, err)
+	msg := wire.Message{
+		Kind: uint16(messages.MessageType_MessageType_Failure),
+		Data: data,
+	}
+	dev.On("SignMessage", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(msg, nil)
+	di := createDeviceInteraction2(dev)
+
+	// When
+	_, err = di.SignMessage(1, 1, "", "").Then(func(data interface{}) interface{} {
+		return data
+	}).Await()
+
+	// Then
+	require.Error(t, err)
+	require.Equal(t, err, errors.New("calling DecodeResponseeSkycoinSignMessage with wrong message type: MessageType_Failure"))
+}
