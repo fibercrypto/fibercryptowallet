e51a14f554e58753ad9622b892ba4e260ec2b97a
Alvaro Denis <dev0003@simelo.tech>
2020-03-04 16:04:28 -0500

    fix number scale in tests ref #139

--- src/coin/skycoin/models/main_test.go
+++ src/coin/skycoin/models/main_test.go
@@ -138,7 +138,7 @@ func TestSkyFiberPluginNetOperations(t *testing.T) {
 		require.Nil(t, err1)
 		require.IsType(t, new(SkycoinBlockchain), api)
 		sAPI := api.(*SkycoinBlockchain)
-		require.Equal(t, uint64(params.DataRefreshTimeout*100000000000000), sAPI.CacheTime)
+		require.Equal(t, uint64(params.DataRefreshTimeout*1000000000), sAPI.CacheTime)
 	})
 }
 
