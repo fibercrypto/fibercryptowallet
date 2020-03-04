40513b2f4df60fa7478f8c987d956164e0998429
Olemis Lang <olemis@simelo.tech>
2020-03-03 19:05:44 -0500

    [sky] refs #139 - Adjust test comparing params.DataRefreshTimeout vs SkycoinBlockchain.CacheTime

--- src/coin/skycoin/models/main_test.go
+++ src/coin/skycoin/models/main_test.go
@@ -8,14 +8,13 @@ import (
 	"sort"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"github.com/fibercrypto/fibercryptowallet/src/coin/mocks"
 	"github.com/fibercrypto/fibercryptowallet/src/core"
-	util "github.com/fibercrypto/fibercryptowallet/src/util"
 	"github.com/fibercrypto/fibercryptowallet/src/params"
+	util "github.com/fibercrypto/fibercryptowallet/src/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
-
 
 //Prepare the mock API for all test
 func TestMain(m *testing.M) {
@@ -139,7 +138,7 @@ func TestSkyFiberPluginNetOperations(t *testing.T) {
 		require.Nil(t, err1)
 		require.IsType(t, new(SkycoinBlockchain), api)
 		sAPI := api.(*SkycoinBlockchain)
-		require.Equal(t, uint64(params.DataRefreshTimeout*1000000000), sAPI.CacheTime)
+		require.Equal(t, uint64(params.DataRefreshTimeout*10000), sAPI.CacheTime)
 	})
 }
 
